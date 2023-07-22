f495e2f0a293ca52188bf9af2e9a03ee84414381
cDc <cdc.seacave@gmail.com>
2023-07-22 12:16:16 +0000

    instant-ngp uniform sampler (not working)

--- nerfstudio/models/instant_ngp.py
+++ nerfstudio/models/instant_ngp.py
@@ -41,13 +41,14 @@ from nerfstudio.model_components.losses import (
     MSELoss,
     orientation_loss,
 )
-from nerfstudio.model_components.ray_samplers import VolumetricSampler
+from nerfstudio.model_components.ray_samplers import UniformSampler
 from nerfstudio.model_components.renderers import (
     AccumulationRenderer,
     DepthRenderer,
     NormalsRenderer,
     RGBRenderer,
 )
+from nerfstudio.model_components.scene_colliders import NearFarCollider
 from nerfstudio.model_components.shaders import NormalsShader
 from nerfstudio.models.base_model import Model, ModelConfig
 from nerfstudio.utils import colormaps
@@ -77,8 +78,8 @@ class InstantNGPModelConfig(ModelConfig):
     """Threshold for opacity skipping."""
     cone_angle: float = 0.004
     """Should be set to 0.0 for blender scenes but 1./256 for real scenes."""
-    render_step_size: Optional[float] = None
-    """Minimum step size for rendering."""
+    num_samples: int = 64
+    """Number of samples per ray."""
     near_plane: float = 0.05
     """How far along ray to start sampling."""
     far_plane: float = 1e3
@@ -128,21 +129,10 @@ class NGPModel(Model):
 
         self.scene_aabb = Parameter(self.scene_box.aabb.flatten(), requires_grad=False)
 
-        if self.config.render_step_size is None:
-            # auto step size: ~1000 samples in the base level grid
-            self.config.render_step_size = ((self.scene_aabb[3:] - self.scene_aabb[:3]) ** 2).sum().sqrt().item() / 1000
-        # Occupancy Grid.
-        self.occupancy_grid = nerfacc.OccGridEstimator(
-            roi_aabb=self.scene_aabb,
-            resolution=self.config.grid_resolution,
-            levels=self.config.grid_levels,
-        )
+        self.sampler = UniformSampler()
 
-        # Sampler
-        self.sampler = VolumetricSampler(
-            occupancy_grid=self.occupancy_grid,
-            density_fn=self.field.density_fn,
-        )
+        # Collider
+        self.collider = NearFarCollider(near_plane=self.config.near_plane, far_plane=self.config.far_plane)
 
         # renderers
         self.renderer_rgb = RGBRenderer(background_color=self.config.background_color)
@@ -164,19 +154,7 @@ class NGPModel(Model):
     def get_training_callbacks(
         self, training_callback_attributes: TrainingCallbackAttributes
     ) -> List[TrainingCallback]:
-        def update_occupancy_grid(step: int):
-            self.occupancy_grid.update_every_n_steps(
-                step=step,
-                occ_eval_fn=lambda x: self.field.density_fn(x) * self.config.render_step_size,
-            )
-
-        return [
-            TrainingCallback(
-                where_to_run=[TrainingCallbackLocation.BEFORE_TRAIN_ITERATION],
-                update_every_num_iters=1,
-                func=update_occupancy_grid,
-            ),
-        ]
+        return []
 
     def get_param_groups(self) -> Dict[str, List[Parameter]]:
         param_groups = {}
@@ -187,61 +165,42 @@ class NGPModel(Model):
 
     def get_outputs(self, ray_bundle: RayBundle):
         assert self.field is not None
-        num_rays = len(ray_bundle)
 
         with torch.no_grad():
-            ray_samples, ray_indices = self.sampler(
-                ray_bundle=ray_bundle,
-                near_plane=self.config.near_plane,
-                far_plane=self.config.far_plane,
-                render_step_size=self.config.render_step_size,
-                alpha_thre=self.config.alpha_thre,
-                cone_angle=self.config.cone_angle,
-            )
+            ray_samples = self.sampler(ray_bundle, num_samples=self.config.num_samples)
 
         field_outputs = self.field.forward(ray_samples, compute_normals=self.config.estimate_normals)
 
-        # accumulation
-        packed_info = nerfacc.pack_info(ray_indices, num_rays)
-        weights = nerfacc.render_weight_from_density(
-            t_starts=ray_samples.frustums.starts[..., 0],
-            t_ends=ray_samples.frustums.ends[..., 0],
-            sigmas=field_outputs[FieldHeadNames.DENSITY][..., 0],
-            packed_info=packed_info,
-        )[0]
-        weights = weights[..., None]
+        weights = ray_samples.get_weights(field_outputs[FieldHeadNames.DENSITY])
 
         rgb = self.renderer_rgb(
             rgb=field_outputs[FieldHeadNames.RGB],
             weights=weights,
-            ray_indices=ray_indices,
-            num_rays=num_rays,
         )
         depth = self.renderer_depth(
-            weights=weights, ray_samples=ray_samples, ray_indices=ray_indices, num_rays=num_rays
+            weights=weights, ray_samples=ray_samples
         )
-        accumulation = self.renderer_accumulation(weights=weights, ray_indices=ray_indices, num_rays=num_rays)
+        accumulation = self.renderer_accumulation(weights=weights)
 
         outputs = {
             "rgb": rgb,
             "accumulation": accumulation,
             "depth": depth,
-            "num_samples_per_ray": packed_info[:, 1],
         }
         if self.config.estimate_normals:
-            outputs["normals"] = self.renderer_normals(normals=field_outputs[FieldHeadNames.NORMAL], weights=weights, ray_indices=ray_indices, num_rays=num_rays)
+            outputs["normals"] = self.renderer_normals(normals=field_outputs[FieldHeadNames.NORMAL], weights=weights)
         if self.training:
             if self.config.estimate_normals:
                 outputs["rendered_orientation_loss"] = orientation_loss(
                     weights.detach(), field_outputs[FieldHeadNames.NORMAL], ray_bundle.directions
                 )
+        # print(f"get_outputs torch.cuda.memory_allocated: {torch.cuda.memory_allocated(0)/1024/1024/1024} GB")
         return outputs
 
     def get_metrics_dict(self, outputs, batch):
         image = batch["image"].to(self.device)
         metrics_dict = {}
         metrics_dict["psnr"] = self.psnr(outputs["rgb"], image)
-        metrics_dict["num_samples_per_batch"] = outputs["num_samples_per_ray"].sum()
         return metrics_dict
 
     def get_loss_dict(self, outputs, batch, metrics_dict=None):
