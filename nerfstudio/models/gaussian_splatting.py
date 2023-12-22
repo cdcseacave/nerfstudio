Fix quaternion calculation and update exporter

nerfstudio/models/gaussian_splatting.py
@@ -32,7 +32,7 @@ from nerfstudio.data.scene_box import OrientedBox
 from nerfstudio.engine.callbacks import TrainingCallback, TrainingCallbackAttributes, TrainingCallbackLocation
 from nerfstudio.engine.optimizers import Optimizers
 from nerfstudio.models.base_model import Model, ModelConfig
-from nerfstudio.utils.math import quaternion_from_normal
+from nerfstudio.utils.math import quaternion_from_vectors
 import math
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
@@ -193,14 +193,14 @@ class GaussianSplattingModel(Model):
             print(f"Initializing {num_points} splats with normals")
             scales = torch.log(torch.cat(
                 [
-                    torch.ones((num_points, 1)) * (1.0 / self.config.max_gauss_ratio),
+                    avg_dist * (1.0 / self.config.max_gauss_ratio),
                     avg_dist,
                     avg_dist,
                 ],
                 dim=-1,
             ))
             quats = torch.from_numpy(
-                np.array([quaternion_from_normal(n).q.astype(np.float32) for n in normals.cpu().numpy()])
+                np.array([quaternion_from_vectors(n).q.astype(np.float32) for n in normals.cpu().numpy()])
             )
         return scales, quats
 

nerfstudio/scripts/exporter.py
@@ -636,12 +636,10 @@ class ExportGaussianSplat(Exporter):
         data['y'] = transformed_positions[1]
         data['z'] = transformed_positions[2]
 
-        transformed_scales = torch.sigmoid(torch.from_numpy(scales))
-        transformed_scales = (transformed_scales / scale_transform).T
-        transformed_scales = torch.logit(transformed_scales).numpy()
-        data['scale_0'] = transformed_scales[0]
-        data['scale_1'] = transformed_scales[1]
-        data['scale_2'] = transformed_scales[2]
+        transformed_scales = np.log(np.exp(scales) / scale_transform)
+        data['scale_0'] = transformed_scales[:, 0]
+        data['scale_1'] = transformed_scales[:, 1]
+        data['scale_2'] = transformed_scales[:, 2]
 
         normals = np.array([data['nx'], data['ny'], data['nz']])
         inv_rotation_transform = np.linalg.inv(rotation_transform)
@@ -661,6 +659,7 @@ class ExportGaussianSplat(Exporter):
 
         with open(filename, mode='wb') as f:
             PlyData([PlyElement.describe(data, 'vertex')]).write(f)
+        CONSOLE.print(f'Wrote {filename}')
 
     def estimate_normals(
         self,

nerfstudio/utils/math.py
@@ -542,20 +542,36 @@ def compute_statistics(
     print(f"\tMin: {min_val}, Mean: {mean_val}, Median: {median_val}, Max: {max_val}")
     print(f"\tStd-dev: {std_dev_val}, Variance: {variance_val}, Skewness: {skewness_val}, Kurtosis: {kurtosis_val}")
 
+def quaternion_to_rotation(quat):
+    """
+    Convert a quaternion into a rotation matrix using pyquaternion format,
+    which uses scalar-first format (w x y z).
+    Args:
+        quat (numpy.ndarray): The quaternion to convert.
+    Returns:
+        numpy.ndarray: The rotation matrix.
+    """
+    # Normalize the quaternion
+    s = 1.0 / np.linalg.norm(quat)
+    w, x, y, z = quat[0] * s, quat[1] * s, quat[2] * s, quat[3] * s
 
-def quaternion_from_normal(normal, up_direction=np.array([1.0, 0.0, 0.0])):
+    # Construct the rotation matrix
+    return np.array([
+        [1.0 - 2.0 * (y**2 + z**2), 2.0 * (x * y - w * z), 2.0 * (x * z + w * y)],
+        [2.0 * (x * y + w * z), 1.0 - 2.0 * (x**2 + z**2), 2.0 * (y * z - w * x)],
+        [2.0 * (x * z - w * y), 2.0 * (y * z + w * x), 1.0 - 2.0 * (x**2 + y**2)]
+    ])
+
+def quaternion_from_vectors(normal, up_direction=np.array([1.0, 0.0, 0.0])):
     """
     Generate a quaternion from a normal vector, assuming a certain 'up' direction.
     For ex if the 'up' direction is [1.0, 0.0, 0.0], then the normal will be the first column of the rotation matrix.
     Args:
-        normal (numpy.ndarray): The normal vector.
+        normal (numpy.ndarray): The normalized normal vector.
         up_direction (numpy.ndarray): The normalized 'up' direction, default is [1.0, 0.0, 0.0].
     Returns:
         Quaternion: The quaternion representing the rotation.
     """
-    # Normalize the input vectors
-    normal = normal / np.linalg.norm(normal)
-
     # Compute the axis of rotation (cross product)
     axis = np.cross(up_direction, normal)
 